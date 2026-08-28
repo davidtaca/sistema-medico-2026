c0610efd78201ccdb688c1d492330c3f8237532b
David Tacatic <dsoct@miumg.edu.gt>
2026-08-28 00:28:55 -0600

    Update SistemaMedicoApplication.java

--- src/main/java/com/hospital/sistemamedico/SistemaMedicoApplication.java
+++ src/main/java/com/hospital/sistemamedico/SistemaMedicoApplication.java
@@ -2,7 +2,7 @@ package com.hospital.sistemamedico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 public class SistemaMedicoApplication {
 
